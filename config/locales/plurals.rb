4b19dfc031d8da948f4e200a3bfff72e68c67253
dnimtsol <lostmind@mail.ru>
2020-03-05 23:43:24 +0300

    hound 3

--- config/locales/plurals.rb
+++ config/locales/plurals.rb
@@ -1,24 +1,20 @@
 { ru:
   { i18n:
     { plural:
-      { key: [:zero, :one, :few, :many],
-        rule: lambda { |n| 
-          if n == 0
-            :zero
-          elsif
-            ( ( n % 10 ) == 1 ) && ( ( n % 100 != 11 ) )
-            :one
-          elsif
-            ( [2, 3, 4].include?(n % 10) \
-            && ![12, 13, 14].include?(n % 100) )
-            :few
-          elsif ( (n % 10) == 0 || \
-            ![5, 6, 7, 8, 9].include?(n % 10) || \
-            ![11, 12, 13, 14].include?(n % 100) )
-            :many
-          end
-                     }
-      }
-    }
-  }
-}
+      { key: %i[zero one few many],
+        rule: lambda { |n|
+                if n == 0
+                  :zero
+                elsif
+                  ((n % 10) == 1) && ((n % 100 != 11))
+                  :one
+                elsif
+                  [2, 3, 4].include?(n % 10) \
+                  && ![12, 13, 14].include?(n % 100)
+                  :few
+                elsif (n % 10) == 0 || \
+                      ![5, 6, 7, 8, 9].include?(n % 10) || \
+                      ![11, 12, 13, 14].include?(n % 100)
+                  :many
+                end
+              } } } } }
