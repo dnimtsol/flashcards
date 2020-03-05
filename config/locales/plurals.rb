3ff1c309127ea73c442e30878d103e20beb0db7e
dnimtsol <lostmind@mail.ru>
2020-03-05 23:53:09 +0300

    hound 5

--- config/locales/plurals.rb
+++ config/locales/plurals.rb
@@ -5,14 +5,12 @@
         rule: lambda { |n|
                 if n == 0
                   :zero
-                elsif
-                  ((n % 10) == 1) && ((n % 100 != 11))
+                elsif ((n % 10) == 1) && ((n % 100 != 11))
                   :one
-                elsif
-                  [2, 3, 4].include?(n % 10) \
+                elsif [2, 3, 4].include?(n % 10) \
                   && ![12, 13, 14].include?(n % 100)
                   :few
-                elsif (n % 10) == 0 || \
+                elsif (n % 10).zero? || \
                       ![5, 6, 7, 8, 9].include?(n % 10) || \
                       ![11, 12, 13, 14].include?(n % 100)
                   :many
