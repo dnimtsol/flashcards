cac7c3a8e06f474d943b26e0428e23c4d0cb34a2
dnimtsol <lostmind@mail.ru>
2020-03-07 01:07:31 +0300

    all text to i18n

--- app/validators/different_validator.rb
+++ app/validators/different_validator.rb
@@ -2,11 +2,13 @@
 
 # DifferentValidator class
 class DifferentValidator < ActiveModel::Validator
-  def hlm(data)
-    data.delete(' ').downcase
-  end
-
   def validate(record)
     record.errors[:base] << I18n.t(:notsame) if hlm(record.original_text) == hlm(record.translated_text)
+  end
+
+  private
+
+  def hlm(data)
+    data.delete(' ').downcase
   end
 end
