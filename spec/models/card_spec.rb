845d5477f04d40599d58e14eaa4990e47132b1fc
dnimtsol <lostmind@mail.ru>
2020-03-13 23:59:40 +0300

    card test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 RSpec.describe Card, type: :model do
   describe 'Card' do
     subject { Card.create(original_text: 'Курица', translated_text: 'Chiken', review_date: Date.today) }
-    %i[original_text translated_text review_date].each do |field|
-      it { should validate_presence_of(field) }
-    end
+
+    it { should validate_presence_of(:original_text) }
+    it { should validate_presence_of(:translated_text) }
+    it { should validate_presence_of(:review_date) }
 
     it { described_class.validators.map(&:class).include? DifferentValidator }
-
     it { is_expected.to be_valid }
 
     it 'correct time ' do
